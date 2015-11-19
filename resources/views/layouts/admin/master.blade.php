e238e5d3887f31a1cecacdb40ea7d82fae95951a
Ben Stapleton <ben@glyvolution.co.uk>
2015-11-19 23:13:00 +0000

    Redid the colour scheme for admin interface, and stripped out the bootstrap grid for the layout.

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -7,36 +7,29 @@
 </head>
 
 <body>
-<div class="container-fluid">
-    <div class="row">
-        @section('admin-sidebar')
-            <div class="col-sm-4 col-md-3">
-                <aside class="admin-sidebar">
-                    <nav>
-                        <ul class="list-unstyled">
-                            <li><a href="{{ action('AdminController@index') }}">Dashboard</a></li>
-                            <li><a href="{{ action('Admin\ArticleController@index') }}">Articles</a></li>
-                            <li><a href="#">Forums</a></li>
-                            <li><a href="#">Comments</a></li>
-                            <li><a href="{{ action('Admin\UserController@index') }}">Users</a></li>
-                            <li><a href="#">Log Out</a></li>
-                        </ul>
-                    </nav>
-                </aside>
-            </div>
-        @show
+@section('admin-sidebar')
+    <aside class="admin-sidebar">
+        <nav>
+            <ul class="list-unstyled admin-nav">
+                <li><a href="{{ action('AdminController@index') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li><a href="{{ action('Admin\ArticleController@index') }}"><i class="fa fa-pencil"></i> Articles</a></li>
+                <li><a href="#"><i class="fa fa-file"></i> Pages</a></li>
+                <li><a href="#"><i class="fa fa-sticky-note"></i> Forums</a></li>
+                <li><a href="#"><i class="fa fa-comment"></i> Comments</a></li>
+                <li><a href="{{ action('Admin\UserController@index') }}"><i class="fa fa-users"></i> Users</a></li>
+                <li class="sign-out"><a href="#"><i class="fa fa-sign-out"></i> Log Out</a></li>
+            </ul>
+        </nav>
+    </aside>
+@show
 
-        <div class="col-sm-8 col-md-9">
-            <section class="admin-content">
-                @yield('content')
-            </section>
+<section class="admin-content">
+    @yield('content')
 
-            <aside class="copyright">
-                Copyright nonsense can go here
-            </aside>
-        </div>
-    </div>
-</div>
+    <aside class="copyright">
+        Copyright nonsense can go here
+    </aside>
+</section>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
